75581e6d19470f53874ebdffc7da6351ca948b88
sora <chuanqi.sun.dev@gmail.com>
2022-06-03 05:50:30 +0000

    add journaled fs wrapper

--- packages/browser-extension/src/workers/services/file-system.ts
+++ packages/browser-extension/src/workers/services/file-system.ts
@@ -1,38 +1,47 @@
 import LightningFS from "@isomorphic-git/lightning-fs";
+import { ObservableFileSystem } from "./observable-file-system";
 
 export class FileSystem {
-  public fs = new LightningFS("tinykb-fs");
+  public fs = new ObservableFileSystem({
+    fsp: new LightningFS().promises,
+    onChange: (record) => {
+      console.log(record);
+    },
+  });
 
-  constructor() {}
+  constructor() {
+    this.fs.init("tinykb-fs");
+  }
 
   async ensureRepo(repoName: string) {
     // TODO refactor with recursion
+    // TODO push down fs level operations into Observable FS class
     try {
-      const rootStat = await this.fs.promises.stat(`/repos`);
+      const rootStat = await this.fs.stat(`/repos`);
       if (!rootStat.isDirectory()) throw new Error(`/repos is not a dir`);
     } catch (error) {
       if ((error as any)?.code === "ENOENT") {
-        await this.fs.promises.mkdir(`/repos/`);
+        await this.fs.mkdir(`/repos/`);
       }
     }
     try {
-      const repoStat = await this.fs.promises.stat(`/repos/${repoName}`);
+      const repoStat = await this.fs.stat(`/repos/${repoName}`);
       if (!repoStat.isDirectory()) throw new Error(`/repos/${repoName} is not a dir`);
     } catch (error) {
       if ((error as any)?.code === "ENOENT") {
-        await this.fs.promises.mkdir(`/repos/${repoName}`);
+        await this.fs.mkdir(`/repos/${repoName}`);
       }
     }
   }
 
   async addFile(repoName: string, path: string, content: string) {
-    await this.fs.promises.writeFile(`/repos/${repoName}/${path}`, content);
+    await this.fs.writeFile(`/repos/${repoName}/${path}`, content);
   }
 
   async listFiles(repoName: string) {
-    const fileNames = await this.fs.promises.readdir(`/repos/${repoName}`);
+    const fileNames = await this.fs.readdir(`/repos/${repoName}`);
     // TODO read from metadata index instead
-    const files = await Promise.all(fileNames.map((fileName) => this.fs.promises.readFile(`/repos/${repoName}/${fileName}`, { encoding: "utf8" })));
+    const files = await Promise.all(fileNames.map((fileName) => this.fs.readFile(`/repos/${repoName}/${fileName}`, { encoding: "utf8" })));
 
     return files as string[];
   }
--- packages/browser-extension/src/workers/services/version-control.ts
+++ packages/browser-extension/src/workers/services/version-control.ts
@@ -2,22 +2,28 @@ import LightningFS from "@isomorphic-git/lightning-fs";
 // import http from "isomorphic-git/http/web";
 import git from "../lib/isomorphic-git/index.umd.min";
 import type { FileSystem } from "./file-system";
+import { ObservableFileSystem } from "./observable-file-system";
 
 export class VersionControl {
-  private fs = new LightningFS("tinykb-fs");
+  private fs = {
+    promises: new ObservableFileSystem({
+      fsp: new LightningFS("tinykb-fs").promises,
+      onChange: () => {},
+    }),
+  };
 
   constructor(private fileSystem: FileSystem) {}
 
   async ensureRepo(repoName: string) {
     try {
       await git.log({
-        fs: this.fileSystem.fs,
+        fs: this.fs,
         dir: `/repos/${repoName}`,
       });
     } catch (error) {
       if (error instanceof git.Errors.NotFoundError) {
         git.init({
-          fs: this.fileSystem.fs,
+          fs: this.fs,
           dir: `/repos/${repoName}`,
         });
       } else {
