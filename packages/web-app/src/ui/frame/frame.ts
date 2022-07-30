48c5fd796ea6bfcf4c893d9ceb036e6effc79cea
Chuanqi Sun <chuanqi.sun.dev@gmail.com>
2022-07-30 16:35:18 -0700

    add prosemirror

--- packages/web-app/src/ui/frame/frame.ts
+++ packages/web-app/src/ui/frame/frame.ts
@@ -1,3 +1,12 @@
+import { exampleSetup } from "prosemirror-example-setup";
+import "prosemirror-example-setup/style/style.css";
+import "prosemirror-menu/style/menu.css";
+import { DOMParser, Schema } from "prosemirror-model";
+import { schema } from "prosemirror-schema-basic";
+import { addListNodes } from "prosemirror-schema-list";
+import { EditorState } from "prosemirror-state";
+import { EditorView } from "prosemirror-view";
+import "prosemirror-view/style/prosemirror.css";
 import { createDelegationHandler } from "../../utils/dom-events";
 import { loadTemplate } from "../../utils/template";
 import "./frame.css";
@@ -6,7 +15,8 @@ const template = loadTemplate(/*html*/ `
 <menu>
   <li><button data-command="save">Save</button></li>
 </menu>
-<div contenteditable="plaintext-only"></div>
+<div id="content" hidden></div>
+<div id="editor"></div>
 `);
 
 declare global {
@@ -16,10 +26,16 @@ declare global {
 }
 
 export class FrameElement extends HTMLElement {
-  private editableElement!: HTMLElement;
+  private contentElement!: HTMLElement;
+  private editorElement!: HTMLElement;
+  private schema = new Schema({
+    nodes: addListNodes(schema.spec.nodes, "paragraph block*", "block"),
+    marks: schema.spec.marks,
+  });
 
   async connectedCallback() {
-    this.editableElement = template.querySelector("[contenteditable]")!;
+    this.contentElement = template.querySelector("#content")!;
+    this.editorElement = template.querySelector("#editor")!;
 
     const handleClick = createDelegationHandler("data-command", {
       save: this.handleSave.bind(this),
@@ -30,13 +46,20 @@ export class FrameElement extends HTMLElement {
   }
 
   load(text: string) {
-    this.editableElement.innerHTML = text;
+    this.contentElement.innerHTML = text;
+
+    new EditorView(this.editorElement, {
+      state: EditorState.create({
+        doc: DOMParser.fromSchema(this.schema).parse(this.contentElement),
+        plugins: exampleSetup({ schema: this.schema }),
+      }),
+    });
   }
 
   private async handleSave() {
     this.dispatchEvent(
       new CustomEvent("saveFrame", {
-        detail: this.editableElement!.innerHTML,
+        detail: this.contentElement!.innerHTML,
       })
     );
   }
