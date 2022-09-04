1a5ffcb963ddc0fa12e918df2cb5b5d517799264
Chuanqi Sun <chuanqi.sun.dev@gmail.com>
2022-09-04 19:58:38 +0000

    remove unused utils

--- apps/web/src/utils/header.ts
+++ apps/web/src/utils/header.ts
@@ -7,49 +7,6 @@ export type EditorFrame = {
 export type EditorFrameHeader = Record<string, any>;
 export type EditorFrameBody = string;
 
-export function generateInitialHeader(): EditorFrameHeader {
-  const nowIso = new Date().toISOString();
-
-  return {
-    dateCreated: nowIso,
-    dateUpdated: nowIso,
-  };
-}
-
-export function getLatestTimestampHeader(header: EditorFrameHeader) {
-  const nowIso = new Date().toISOString();
-
-  return {
-    ...header,
-    dateUpdated: nowIso,
-  };
-}
-
-export function transformHeaderBeforeCreate(frame: EditorFrame) {
-  const nowIso = new Date().toISOString();
-
-  return {
-    ...frame,
-    header: {
-      ...frame.header,
-      dateCreated: nowIso,
-      dateUpdated: nowIso,
-    },
-  };
-}
-
-export function transformHeaderBeforeUpdate(frame: EditorFrame) {
-  const nowIso = new Date().toISOString();
-
-  return {
-    ...frame,
-    header: {
-      ...frame.header,
-      dateUpdated: nowIso,
-    },
-  };
-}
-
 export function getSchemaHeaderFromEditorHeader(editorHeader: EditorFrameHeader) {
   return {
     ...editorHeader,
