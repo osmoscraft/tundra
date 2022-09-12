55d9b0a9c91489821e4cf3c88f898eb889fe04fe
Chuanqi Sun <chuanqi.sun.dev@gmail.com>
2022-09-12 00:33:49 +0000

    render suggestion

--- apps/modular-web/src/features/command/command.ts
+++ apps/modular-web/src/features/command/command.ts
@@ -7,4 +7,7 @@ export function getForm(e: Event) {
   return e.target as HTMLFormElement;
 }
 
-export function getSuggestions() {}
+export type SuggestHandler = (command: string) => Promise<string | false>;
+export function createSuggester(handlers: SuggestHandler[]) {
+  return async (command: string) => (await Promise.all(handlers.map((handler) => handler(command)))).filter((value) => value !== false) as string[];
+}
--- apps/modular-web/src/main.ts
+++ apps/modular-web/src/main.ts
@@ -1,4 +1,4 @@
-import { getForm, tapOnCommand } from "./features/command/command";
+import { createSuggester, getForm, tapOnCommand } from "./features/command/command";
 import "./features/console/console";
 import { log } from "./features/console/console";
 
@@ -9,23 +9,31 @@ import { closestForm, formData, getFormField, resetForm } from "./utils/form";
 export default function main() {
   const commandDialog = document.getElementById("command-dialog") as HTMLDialogElement;
   const commandForm = document.getElementById("command-form") as HTMLFormElement;
+  const commandSuggestions = document.getElementById("command-suggestions") as HTMLUListElement;
   const consoleElement = document.getElementById("console") as HTMLOutputElement;
 
   const logToConsole = log.bind(null, consoleElement);
   const handleCommandSubmit = (e: Event) => tapOnCommand(getForm(preventDefault(e)), logToConsole).reset();
 
-  const handleCommandSuggest = (e: Event) => {
-    getFormField("comand", formData(closestForm(e.target as Element)!)) as string;
+  const suggest = createSuggester([
+    async (command) => (!command.length ? `Recent` : false),
+    async (command) => (command.length && "sandbox".startsWith(command) ? `<a href="/sandbox.html">Open editor sandbox</a>` : false),
+  ]);
+
+  const getCommand = (e: Event) => getFormField("command", formData(closestForm(e.target as Element)!)) as string;
+  const handleCommandSuggest = async (command: string) => {
+    commandSuggestions.innerHTML = (await suggest(command)).map((item) => `<li>${item}</li>`).join("");
   };
 
   commandForm.addEventListener("submit", handleCommandSubmit);
-  commandForm.addEventListener("input", handleCommandSuggest);
+  commandForm.addEventListener("input", (e) => handleCommandSuggest(getCommand(e)));
 
   window.addEventListener("keydown", (e) => {
     if (e.code === "KeyK" && e.ctrlKey && !e.shiftKey && !e.altKey && !e.metaKey) {
       preventDefault(e);
-      commandDialog.showModal();
       resetForm(commandForm);
+      handleCommandSuggest("");
+      commandDialog.showModal();
     }
   });
 }
