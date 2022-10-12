b0dfd55c6c1593b7895ba2861e4f20339f66f0b9
chuanqisun <chuanqi.sun.dev@gmail.com>
2022-10-12 04:46:38 +0000

    add keyboard handler

---
+++ apps/web/src/features/keyboard/shortcuts.ts
@@ -0,0 +1,16 @@
+export type Shortcut = [
+  keygram: string,
+  /** Not implemented */
+  context: string,
+  handler: (e: KeyboardEvent) => any
+];
+
+export function handleKeydownWithShortcut(shortcuts: Shortcut[], event: KeyboardEvent) {
+  const keygram = getKeygram(event);
+  shortcuts.find((shortcut) => shortcut[0] === keygram)?.[2](event);
+}
+
+/** Format: [Ctrl-][Alt-][Shift-]keyCode */
+function getKeygram(e: KeyboardEvent): string {
+  return `${e.ctrlKey ? "Ctrl-" : ""}${e.altKey ? "Alt-" : ""}${e.shiftKey ? "Shift-" : ""}${e.code}`;
+}
--- apps/web/src/main.ts
+++ apps/web/src/main.ts
@@ -1,4 +1,8 @@
-import "./style.css";
+import { handleKeydownWithShortcut, Shortcut } from "./features/keyboard/shortcuts";
+import "./main.css";
+
+const shortcuts: Shortcut[] = [["Ctrl-KeyK", "", console.log]];
+window.addEventListener("keydown", handleKeydownWithShortcut.bind(null, shortcuts));
 
 async function main() {}
 
