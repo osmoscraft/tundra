3788a998a8d51450092f7779cfe4393d99c5408a
chuanqisun <chuanqi.sun.dev@gmail.com>
2022-10-12 07:21:23 +0000

    add ref docs

--- apps/web/src/utils/functional/demethodize.ts
+++ apps/web/src/utils/functional/demethodize.ts
@@ -1,4 +1,5 @@
 import type { ParametersTypeOrNever, ReturnTypeOrNever } from "./type";
 
+// ref: http://intelligiblebabble.com/clever-way-to-demethodize-native-js-methods/
 export const demethodize = Function.prototype.bind.bind(Function.prototype.call);
 export type Demethodized<P, Method extends keyof P> = <T extends P>(object: T, ...args: ParametersTypeOrNever<T[Method]>) => ReturnTypeOrNever<T[Method]>;
