f9c8ed3b562ce960ae409b2c1eada95e7f73798b
chuanqisun <chuanqi.sun.dev@gmail.com>
2022-10-15 05:24:20 +0000

    add a basic router

--- apps/web/src/main.ts
+++ apps/web/src/main.ts
@@ -1,12 +1,21 @@
 import { handleKeydownWithShortcut, Shortcut } from "./features/keyboard/shortcuts";
+import { getInternalHrefFromClick, onPopState, pushUrl, routeSubject, selectInternalHrefClick } from "./features/router/router";
 import "./main.css";
 import { preventDefault } from "./utils/dom/event";
-import { pipe } from "./utils/functional/pipe";
+import { nullablePipe, pipe } from "./utils/functional/pipe";
 import { tap } from "./utils/functional/tap";
 
-const shortcuts: Shortcut[] = [["Ctrl-KeyK", "", pipe(tap(console.log), preventDefault)]];
-window.addEventListener("keydown", pipe(handleKeydownWithShortcut.bind(null, shortcuts)));
+async function main() {
+  window.addEventListener("click", nullablePipe(selectInternalHrefClick, preventDefault, getInternalHrefFromClick, pushUrl));
+  window.addEventListener("popstate", onPopState);
 
-async function main() {}
+  routeSubject.addEventListener("afterRouteChange", () => {
+    const id = new URLSearchParams(window.location.search).get("id");
+    console.log(id);
+  });
+
+  const shortcuts: Shortcut[] = [["Ctrl-KeyK", "", pipe(tap(console.log), preventDefault)]];
+  window.addEventListener("keydown", pipe(handleKeydownWithShortcut.bind(null, shortcuts)));
+}
 
 main();
--- apps/web/src/utils/dom/event.ts
+++ apps/web/src/utils/dom/event.ts
@@ -1,4 +1,8 @@
-export const preventDefault = (event: Event) => {
+export function preventDefault(event: Event) {
   event.preventDefault();
   return event;
-};
+}
+
+export function closestTarget<T extends Element>(selector: string, event: Event): T | null {
+  return (event.target as Element)?.closest(selector) ?? null;
+}
