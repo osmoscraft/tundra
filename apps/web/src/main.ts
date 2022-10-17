2ed6863e9e86a5391c8fb2894a4f9a1f0ce084c8
Chuanqi Sun <chuanqi.sun.dev@gmail.com>
2022-10-17 19:49:48 +0000

    test immutability

--- apps/web/src/main.ts
+++ apps/web/src/main.ts
@@ -1,20 +1,24 @@
 import { Nomad } from "./utils/dom";
 
 export async function main() {
-  // console.log([...chain((node: Node) => node, document.querySelectorAll("*"))]);
-  console.log([
-    ...Nomad.$("*")
-      .chain((node, index) => {
-        node.setAttribute("data-i", index.toString());
-        return node;
-      })
-      .chain((node) => node.querySelectorAll("div,section,p,a"))
-      // .chain((node) => node.querySelectorAll("button"))
-      .chain((node) => node.querySelectorAll("*")),
-  ]);
+  // // console.log([...chain((node: Node) => node, document.querySelectorAll("*"))]);
+  // console.log([
+  //   ...Nomad.$("*")
+  //     .chain((node, index) => {
+  //       node.setAttribute("data-i", index.toString());
+  //       return node;
+  //     })
+  //     .chain((node) => node.querySelectorAll("div,section,p,a"))
+  //     // .chain((node) => node.querySelectorAll("button"))
+  //     .chain((node) => node.querySelectorAll("*")),
+  // ]);
 
-  const results = [...Nomad.$<HTMLButtonElement>("button")];
-  console.log(results);
+  // const results = [...Nomad.$<HTMLButtonElement>("button")];
+  // console.log(results);
+
+  const base = Nomad.$("*").chain((node) => node.querySelectorAll("*"));
+  console.log([...base]);
+  console.log([...base]);
 }
 
 main();
