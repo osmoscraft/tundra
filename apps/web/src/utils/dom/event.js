1ac7790cd63826324134ce1d56806a27113e0189
chuanqisun <chuanqi.sun.dev@gmail.com>
2022-10-22 19:44:18 +0000

    augment typing

--- apps/web/src/utils/dom/event.js
+++ apps/web/src/utils/dom/event.js
@@ -1,6 +1,11 @@
-export const emit = (name, init, target = window) => target.dispatchEvent(new CustomEvent(name, init));
-export const on = (name, handler, target = window) => (target.addEventListener(name, handler), target);
-export const off = (name, handler, target = window) => (target.removeEventListener(name, handler), target);
+export const emit = (/** @type {keyof WindowEventMap} */ name, init, target = window) =>
+  target.dispatchEvent(new CustomEvent(name, init));
+export const on = (/** @type {keyof WindowEventMap} */ name, handler, target = window) => (
+  target.addEventListener(name, handler), target
+);
+export const off = (/** @type {keyof WindowEventMap} */ name, handler, target = window) => (
+  target.removeEventListener(name, handler), target
+);
 
 export const preventDefault = (e) => (e.preventDefault(), e);
 export const stopPropagation = (e) => (e.stopPropagation(), e);
--- apps/web/src/utils/dom/factory.js
+++ apps/web/src/utils/dom/factory.js
@@ -4,10 +4,7 @@ export const templateFromHtml = (html) => {
   return t;
 };
 
-/**
- * @type {typeof document.createElement}
- */
-export const h = (tag) => document.createElement(tag);
+export const h = (/** @type {Parameters<typeof document.createElement>} */ ...tag) => document.createElement(...tag);
 
 export const attachShadowHtml = (html, target) => attachShadowTemplate(templateFromHtml(html), target);
 export const fragmentFromHtml = (html) => cloneTemplateContent(templateFromHtml(html));
