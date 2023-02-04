95acd7c9bf6ff570707bcc991367e75721983db0
chuanqisun <chuanqi.sun.dev@gmail.com>
2023-02-04 21:32:22 +0000

    DB reset and download

--- apps/web-extension/src/pages/popup.ts
+++ apps/web-extension/src/pages/popup.ts
@@ -1,7 +1,26 @@
 import "./popup.css";
 
 export default async function main() {
-  const worker = new Worker("./worker.js", { type: "module" });
+  const worker = new Worker("./worker.js?sqlite3.dir=sqlite3");
+
+  document.querySelector("#download")!.addEventListener("click", () => {
+    worker.postMessage({ name: "request-download" });
+  });
+
+  document.querySelector("#reset")!.addEventListener("click", () => {
+    worker.postMessage({ name: "request-reset" });
+  });
+
+  worker.addEventListener("message", async (msg) => {
+    if (msg.data?.name === "file-download-ready") {
+      const handle: FileSystemFileHandle = await (window as any).showSaveFilePicker({
+        suggestedName: "debug.db",
+      });
+      const writable = await (handle as any).createWritable();
+      await writable.write(msg.data.file);
+      await writable.close();
+    }
+  });
 }
 
 main();
--- apps/web-extension/src/worker.ts
+++ apps/web-extension/src/worker.ts
@@ -1,14 +1,82 @@
 declare const self: DedicatedWorkerGlobalScope;
 
-console.log("[worker] online");
+const start = async function (sqlite3: any) {
+  const log = console.log.bind(console);
 
-if (self.crossOriginIsolated) {
-  console.log("[worker] OK: crossOriginIsolated");
-} else {
-  console.error("[worker] Disabled: crossOriginIsolated");
+  const capi = sqlite3.capi; /*C-style API*/
+  const oo = sqlite3.oo1; /*high-level OO API*/
+  log("sqlite3 version", capi.sqlite3_libversion(), capi.sqlite3_sourceid());
+
+  let db;
+  if (sqlite3.opfs) {
+    db = new sqlite3.opfs.OpfsDb("/mydb.sqlite3");
+    log("The OPFS is available.");
+  } else {
+    throw new Error("OPFS is not available");
+  }
+
+  try {
+    console.log(db); // only localStorage size
+    log("Create a table...");
+    db.exec("CREATE TABLE IF NOT EXISTS t(a,b)");
+    log("Insert some data using exec()...");
+    let i;
+    for (i = 20; i <= 25; ++i) {
+      db.exec({
+        sql: "INSERT INTO t(a,b) VALUES (?,?)",
+        bind: [i, i * 2],
+      });
+    }
+    log("Query data with exec() using rowMode 'array'...");
+    db.exec({
+      sql: "SELECT a FROM t ORDER BY a LIMIT 10",
+      rowMode: "array", // 'array' (default), 'object', or 'stmt'
+      callback: function (row: any) {
+        log("row =", row);
+      }.bind({ counter: 0 }),
+    });
+  } finally {
+    db.close();
+  }
+};
+
+self.addEventListener("message", async (event) => {
+  console.log(event);
+  // download db
+  if (event.data?.name === "request-download") {
+    const root = await navigator.storage.getDirectory();
+    const dbFileHandle = await await root.getFileHandle("mydb.sqlite3");
+    const file = await dbFileHandle.getFile();
+    self.postMessage({ name: "file-download-ready", file });
+  } else if (event.data?.name === "request-reset") {
+    const root = await navigator.storage.getDirectory();
+    await root.removeEntry("mydb.sqlite3");
+  }
+});
+
+async function main() {
+  console.log("[worker] online");
+
+  if (self.crossOriginIsolated) {
+    importScripts("sqlite3/sqlite3.js");
+  } else {
+    console.error("[worker] Disabled: crossOriginIsolated");
+  }
+
+  (self as any)
+    .sqlite3InitModule({
+      print: console.log,
+      printErr: console.error,
+    })
+    .then(function (sqlite3: any) {
+      console.log("Done initializing. Running demo...");
+      try {
+        start(sqlite3);
+      } catch (e: any) {
+        console.error("Exception:", e.message);
+      }
+    });
 }
-
-async function main() {}
 
 main();
 
