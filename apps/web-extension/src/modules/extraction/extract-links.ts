820e742417839124963d17492340153908ddfb9a
chuanqisun <chuanqi.sun.dev@gmail.com>
2023-02-05 23:40:53 +0000

    refactor

---
+++ apps/web-extension/src/modules/extraction/extract-links.ts
@@ -0,0 +1,34 @@
+export interface Extraction {
+  title: string;
+  urls: string;
+  target_urls: { title: string; url: string }[];
+}
+
+export function extractLinks(): Extraction {
+  const emptyTextToNull = (text?: string | null) => (text?.trim().length ? text.trim() : null);
+  const title =
+    emptyTextToNull(document.querySelector(`meta[property="og:title"]`)?.getAttribute("content")) ??
+    emptyTextToNull(document.querySelector("title")?.textContent) ??
+    "Untitled";
+  const urls = emptyTextToNull(document.querySelector(`link[rel="canonical"]`)?.getAttribute("href")) ?? location.href;
+  const target_anchors = [...document.querySelectorAll<HTMLAnchorElement>("a:not(:where(nav,header,footer) *)")];
+  const target_urls = target_anchors
+    .map((anchor) => {
+      try {
+        return { title: anchor.innerText.trim(), url: new URL(anchor.href) };
+      } catch {
+        return { title: "", url: null as any as URL };
+      }
+    })
+    .filter((link) => link.title.length > 10 && link.url.protocol.startsWith("http"))
+    .filter((link) => link.url.host !== location.host) // external links only
+    .map((link) => ({ title: link.title, url: link.url.href }))
+    .filter((link, index, array) => array.findIndex((otherLink) => otherLink.url === link.url) === index); // deduplicate
+
+  console.log("[content-script] extracted ", target_urls);
+  return {
+    title,
+    urls,
+    target_urls,
+  };
+}
--- apps/web-extension/src/pages/popup.ts
+++ apps/web-extension/src/pages/popup.ts
@@ -1,12 +1,12 @@
+import { extractLinks } from "../modules/extraction/extract-links";
+import { getActiveTab } from "../utils/get-active-tab";
 import "./popup.css";
 
 export default async function main() {
   const worker = new Worker("./worker.js", { type: "module" });
-  const getActiveTab = () => chrome.tabs.query({ active: true, currentWindow: true });
-  const on = (selector: string, type: string, callback: (e: Event) => any) =>
-    document.querySelector(selector)?.addEventListener(type, callback);
+  const captureForm = document.querySelector<HTMLFormElement>("#capture-form")!;
 
-  on("#capture-form", "submit", (e) => {
+  captureForm.addEventListener("submit", (e) => {
     e.preventDefault();
 
     const captureForm = document.querySelector<HTMLFormElement>("#capture-form")!;
@@ -27,7 +27,7 @@ export default async function main() {
       performance.mark("linkExtractionStart");
       return chrome.scripting.executeScript({
         target: { tabId: activeTab.id },
-        func: extractFinks,
+        func: extractLinks,
       });
     })
     .then((results) => {
@@ -45,41 +45,6 @@ export default async function main() {
         )
         .join("");
     });
-}
-
-export interface Extraction {
-  title: string;
-  urls: string;
-  target_urls: { title: string; url: string }[];
-}
-
-function extractFinks(): Extraction {
-  const emptyTextToNull = (text?: string | null) => (text?.trim().length ? text.trim() : null);
-  const title =
-    emptyTextToNull(document.querySelector(`meta[property="og:title"]`)?.getAttribute("content")) ??
-    emptyTextToNull(document.querySelector("title")?.textContent) ??
-    "Untitled";
-  const urls = emptyTextToNull(document.querySelector(`link[rel="canonical"]`)?.getAttribute("href")) ?? location.href;
-  const target_anchors = [...document.querySelectorAll<HTMLAnchorElement>("a:not(:where(nav,header,footer) *)")];
-  const target_urls = target_anchors
-    .map((anchor) => {
-      try {
-        return { title: anchor.innerText.trim(), url: new URL(anchor.href) };
-      } catch {
-        return { title: "", url: null as any as URL };
-      }
-    })
-    .filter((link) => link.title.length > 10 && link.url.protocol.startsWith("http"))
-    .filter((link) => link.url.host !== location.host) // external links only
-    .map((link) => ({ title: link.title, url: link.url.href }))
-    .filter((link, index, array) => array.findIndex((otherLink) => otherLink.url === link.url) === index); // deduplicate
-
-  console.log("[content-script] extracted ", target_urls);
-  return {
-    title,
-    urls,
-    target_urls,
-  };
 }
 
 main();
