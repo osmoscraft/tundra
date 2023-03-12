5e06a9a4605e1d53735b95dd1ab1c9621c3efed0
chuanqisun <chuanqi.sun.dev@gmail.com>
2023-03-12 03:54:04 +0000

    display links

--- apps/web-extension/src/modules/editor/editor-element.ts
+++ apps/web-extension/src/modules/editor/editor-element.ts
@@ -5,11 +5,16 @@ export interface EditorData {
   path: string;
   title: string;
   url: string;
+  links: {
+    title: string;
+    url: string;
+  }[];
 }
 
 export class EditorElement extends HTMLElement {
   shadowRoot = attachShadowHtml(template, this);
   private form = this.shadowRoot.querySelector("form")!;
+  private linkList = this.shadowRoot.getElementById("link-list") as HTMLUListElement;
 
   connectedCallback() {}
 
@@ -17,5 +22,12 @@ export class EditorElement extends HTMLElement {
     this.form.querySelector<HTMLInputElement>("#path")!.value = data.path;
     this.form.querySelector<HTMLInputElement>("#url")!.value = data.url;
     this.form.querySelector<HTMLInputElement>("#title")!.value = data.title;
+    this.linkList!.innerHTML = data.links
+      .map(
+        (url) => /*html*/ `
+      <li><a href="${url.url}" target="_blank">${url.title}</a></li>
+    `
+      )
+      .join("");
   }
 }
