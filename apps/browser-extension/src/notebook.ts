810d9714261bc2b476abef69c67c735539e385b9
chuanqisun <chuanqi.sun.dev@gmail.com>
2023-05-13 16:08:57 +0000

    file reorg

--- apps/browser-extension/src/notebook.ts
+++
@@ -1,8 +0,0 @@
-import { preloadDbWorker } from "./db-worker-proxy";
-import { EditorElement } from "./modules/edit/editor-element";
-import { OmniboxElement } from "./modules/graph/omnibox/omnibox-element";
-
-const dbWorker = preloadDbWorker();
-
-customElements.define("omnibox-element", OmniboxElement);
-customElements.define("editor-element", EditorElement);
