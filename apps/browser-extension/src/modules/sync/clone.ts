42c36c2fd3bda2daf6e2fa622cc12e2e8af64696
chuanqisun <chuanqi.sun.dev@gmail.com>
2023-05-23 03:31:37 +0000

    remove legacy clone logic

--- apps/browser-extension/src/modules/sync/clone.ts
+++ apps/browser-extension/src/modules/sync/clone.ts
@@ -1,6 +1,6 @@
 import { getConnection } from ".";
 import * as github from "./github";
-import { githubPathToLocalPath, zipPathToGitHubFilePath } from "./path";
+import { zipPathToGitHubFilePath } from "./path";
 import { RemoteChangeStatus, type RemoteChangeRecord } from "./remote-change-record";
 
 export interface CloneParameters {
@@ -24,26 +24,11 @@ export async function* iterateGitHubArchive(zipballUrl: string): AsyncGenerator<
   const itemsGenerator = github.downloadZip(zipballUrl);
   const now = new Date().toISOString();
 
+  performance.mark("clone-start");
   for await (const item of itemsGenerator) {
     const githubPath = zipPathToGitHubFilePath(item.path);
+    console.log(`[clone] path ${githubPath}`);
     yield { path: githubPath, timestamp: now, status: RemoteChangeStatus.Added, readText: () => item.readAsText() };
   }
-}
-
-export async function* importGithubArchive(zipballUrl: string): AsyncGenerator<GitHubItem> {
-  const itemsGenerator = github.downloadZip(zipballUrl);
-
-  performance.mark("import-start");
-  for await (const item of itemsGenerator) {
-    const githubPath = zipPathToGitHubFilePath(item.path);
-    const localPath = githubPathToLocalPath(githubPath);
-    if (!localPath) {
-      console.log(`[import] skip ${githubPath}`);
-    } else {
-      const content = await item.readAsText();
-      console.log(`[import] accept ${localPath} (size: ${content.length})`);
-      yield { path: localPath, content };
-    }
-  }
-  console.log("[perf] import", performance.measure("import duration", "import-start").duration);
+  console.log("[perf] clone", performance.measure("import duration", "import-start").duration);
 }
