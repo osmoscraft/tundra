edfd96d9bebfb61173621eec4efd67ad0585497f
chuanqisun <chuanqi.sun.dev@gmail.com>
2023-05-26 06:24:32 +0000

    naming adjustment

--- apps/browser-extension/src/modules/file-system/sql/schema.sql
+++ apps/browser-extension/src/modules/file-system/sql/schema.sql
@@ -1,11 +1,11 @@
 CREATE TABLE IF NOT EXISTS File (
-  path       TEXT PRIMARY KEY,
-  type       TEXT,
-  content    TEXT NOT NULL,
-  createdAt  TEXT NOT NULL DEFAULT (strftime('%Y-%m-%dT%H:%M:%fZ', 'now')),
-  updatedAt  TEXT NOT NULL DEFAULT (strftime('%Y-%m-%dT%H:%M:%fZ', 'now'))
+  path        TEXT PRIMARY KEY,
+  type        TEXT,
+  content     TEXT NOT NULL,
+  createdTime TEXT NOT NULL DEFAULT (strftime('%Y-%m-%dT%H:%M:%fZ', 'now')),
+  updatedTime TEXT NOT NULL DEFAULT (strftime('%Y-%m-%dT%H:%M:%fZ', 'now'))
 );
 
 CREATE TRIGGER IF NOT EXISTS FileTimestampTrigger AFTER UPDATE ON File FOR EACH ROW BEGIN
-  UPDATE File SET updatedAt = strftime('%Y-%m-%dT%H:%M:%fZ', 'now') WHERE path = old.path;
+  UPDATE File SET updatedTime = strftime('%Y-%m-%dT%H:%M:%fZ', 'now') WHERE path = old.path;
 END;
