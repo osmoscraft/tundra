cf92407318d48ca6c063fdf7a594c194f8a17575
chuanqisun <chuanqi.sun.dev@gmail.com>
2023-05-29 20:55:24 +0000

    add smoke test

---
+++ apps/browser-extension/src/index.test.ts
@@ -0,0 +1,8 @@
+import * as assert from "node:assert";
+import { describe, it } from "node:test";
+
+describe("smoke test", () => {
+  it("should pass", () => {
+    assert.strictEqual(true, true);
+  });
+});
