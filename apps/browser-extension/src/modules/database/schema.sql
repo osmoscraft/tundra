706eda5039f76ec0596842753e3b75fb28a40739
chuanqisun <chuanqi.sun.dev@gmail.com>
2023-06-25 22:09:41 +0000

    remove unused sql

--- apps/browser-extension/src/modules/database/schema.sql
+++ apps/browser-extension/src/modules/database/schema.sql
@@ -51,33 +51,3 @@ CREATE TABLE IF NOT EXISTS Object (
   path TEXT PRIMARY KEY,
   data TEXT
 );
-
--- Graph
-CREATE TABLE IF NOT EXISTS Node (
-  data  TEXT,
-  -- virtual columns from JSON extractions
-  path  TEXT GENERATED ALWAYS AS (json_extract(data, '$.path')) NOT NULL UNIQUE,
-  title TEXT GENERATED ALWAYS AS (json_extract(data, '$.title')) NOT NULL
-  -- future columns
-  -- links
-  -- tags
-);
-
-CREATE VIRTUAL TABLE IF NOT EXISTS NodeFts USING fts5(path, title, content=Node);
-
-CREATE TRIGGER IF NOT EXISTS NodeFtsAfterInsertTrigger AFTER INSERT ON Node BEGIN
-    INSERT INTO NodeFts(rowid, path, title)
-    VALUES (new.rowid, new.path, new.title);
-END;
-
-CREATE TRIGGER IF NOT EXISTS NodeFtsAfterDeleteTrigger AFTER DELETE ON Node BEGIN
-  INSERT INTO NodeFts(NodeFts, rowid, path, title)
-  VALUES('delete', old.rowid, old.path, old.title);
-END;
-
-CREATE TRIGGER IF NOT EXISTS NodeFtsAfterUpdateTrigger AFTER UPDATE ON Node BEGIN
-  INSERT INTO NodeFts( NodeFts, rowid, path, title)
-  VALUES('delete', old.rowid, old.path, old.title);
-  INSERT INTO NodeFts(rowid, path, title)
-  VALUES (new.rowid, new.path, new.title);
-END;
