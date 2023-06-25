a70cd9517b14344ca30b5ddfb441754b7a949c22
chuanqisun <chuanqi.sun.dev@gmail.com>
2023-06-25 00:47:31 +0000

    Undo meta extension

--- apps/browser-extension/src/modules/database/schema.sql
+++ apps/browser-extension/src/modules/database/schema.sql
@@ -5,10 +5,8 @@ CREATE TABLE IF NOT EXISTS File (
   localUpdatedTime TEXT,
   remoteContent TEXT,
   remoteUpdatedTime TEXT,
-  meta TEXT,
 
   /* Drived columns */
-  title TEXT GENERATED ALWAYS AS (json_extract(meta, '$.title')),
   updatedTime TEXT GENERATED ALWAYS AS (
     CASE
       WHEN ifnull(localUpdatedTime, 0) > ifnull(remoteUpdatedTime, 0) THEN localUpdatedTime
@@ -27,23 +25,23 @@ CREATE TABLE IF NOT EXISTS File (
 
 CREATE INDEX IF NOT EXISTS FileUpdatedTimeIdx ON File(updatedTime);
 
-CREATE VIRTUAL TABLE IF NOT EXISTS FileFts USING fts5(path, content, meta, content=File);
+CREATE VIRTUAL TABLE IF NOT EXISTS FileFts USING fts5(path, content, content=File);
 
 CREATE TRIGGER IF NOT EXISTS FileFtsAfterInsertTrigger AFTER INSERT ON File BEGIN
-    INSERT INTO FileFts(rowid, path, content, meta)
-    VALUES (new.rowid, new.path, new.content, new.meta);
+    INSERT INTO FileFts(rowid, path, content)
+    VALUES (new.rowid, new.path, new.content);
 END;
 
 CREATE TRIGGER IF NOT EXISTS FileFtsAfterDeleteTrigger AFTER DELETE ON File BEGIN
-  INSERT INTO FileFts(FileFts, rowid, path, content, meta)
-  VALUES('delete', old.rowid, old.path, old.content, old.meta);
+  INSERT INTO FileFts(FileFts, rowid, path, content)
+  VALUES('delete', old.rowid, old.path, old.content);
 END;
 
 CREATE TRIGGER IF NOT EXISTS FileFtsAfterUpdateTrigger AFTER UPDATE ON File BEGIN
-  INSERT INTO FileFts( FileFts, rowid, path, content, meta)
-  VALUES('delete', old.rowid, old.path, old.content, old.meta);
-  INSERT INTO FileFts(rowid, path, content, meta)
-  VALUES (new.rowid, new.path, new.content, new.meta);
+  INSERT INTO FileFts( FileFts, rowid, path, content)
+  VALUES('delete', old.rowid, old.path, old.content);
+  INSERT INTO FileFts(rowid, path, content)
+  VALUES (new.rowid, new.path, new.content);
 END;
 
 -- Object
