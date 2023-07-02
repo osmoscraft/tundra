4c6b4ad13e9799c7646fab2151fab9df3e464cc2
chuanqisun <chuanqi.sun.dev@gmail.com>
2023-07-02 21:23:35 +0000

    rebuild schema for each test

--- apps/browser-extension/src/modules/database/tests/fixture.ts
+++ apps/browser-extension/src/modules/database/tests/fixture.ts
@@ -2,14 +2,22 @@ import { sqlite3Mem } from "@tinykb/sqlite-utils";
 
 let db: Sqlite3.DB | undefined;
 export async function createTestDb(schema: string) {
-  db ??= await createTestDbInternal(schema);
-  db.exec(`DELETE FROM File;`);
+  db ??= await createEmptyDb();
+  db.exec(`
+  DROP TABLE IF EXISTS File;
+  DROP INDEX IF EXISTS IsDirtyIdx;
+  DROP INDEX IF EXISTS UpdatedAtIdx;
+  DROP TABLE IF EXISTS FileFts;
+  DROP TRIGGER IF EXISTS FileFtsAfterInsertTrigger;
+  DROP TRIGGER IF EXISTS FileFtsAfterDeleteTrigger;
+  DROP TRIGGER IF EXISTS FileFtsAfterUpdateTrigger;
+  `);
+  db.exec(schema);
 
   return db;
 }
 
-export async function createTestDbInternal(schema: string) {
+export async function createEmptyDb() {
   const db = await sqlite3Mem("./sqlite3/jswasm/");
-  db.exec(schema);
   return db;
 }
