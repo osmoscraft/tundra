525c0dbaee4d877634eec552810f2bf02f7e73ea
chuanqisun <chuanqi.sun.dev@gmail.com>
2023-07-02 21:16:48 +0000

    reuse DB for testing

--- apps/browser-extension/src/modules/database/tests/fixture.ts
+++ apps/browser-extension/src/modules/database/tests/fixture.ts
@@ -1,6 +1,14 @@
 import { sqlite3Mem } from "@tinykb/sqlite-utils";
 
+let db: Sqlite3.DB | undefined;
 export async function createTestDb(schema: string) {
+  db ??= await createTestDbInternal(schema);
+  db.exec(`DELETE FROM File;`);
+
+  return db;
+}
+
+export async function createTestDbInternal(schema: string) {
   const db = await sqlite3Mem("./sqlite3/jswasm/");
   db.exec(schema);
   return db;
