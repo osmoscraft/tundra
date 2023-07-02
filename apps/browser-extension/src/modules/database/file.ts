5f7a36ef296e43c9934681d9ad3025f180868fca
chuanqisun <chuanqi.sun.dev@gmail.com>
2023-07-02 06:29:41 +0000

    fs refactor

--- apps/browser-extension/src/modules/database/file.ts
+++ apps/browser-extension/src/modules/database/file.ts
@@ -1,5 +1,23 @@
 import { arrayToParams, paramsToBindings } from "@tinykb/sqlite-utils";
-import type { DbFile, DbFileInternal } from "./schema";
+import type { DbFile, DbFileInternal, DbFileWritable } from "./schema";
+
+export type DbFileRemoteWritable = Omit<DbFileWritable, "localContent" | "localUpdatedAt">;
+export type DbFileLocalWritable = Omit<DbFileWritable, "remoteContent" | "remoteUpdatedAt">;
+export function write(db: Sqlite3.DB, files: DbFileWritable[] | DbFileLocalWritable[] | DbFileRemoteWritable[]) {
+  if (!files.length) return;
+
+  const cols = Object.keys(files[0]);
+
+  const sql = `
+INSERT INTO File (${cols.join(",")}) VALUES
+${files.map((_, i) => `(${cols.map((col) => `:${col}${i}`).join(",")})`).join(",")}
+ON CONFLICT(path) DO UPDATE SET ${cols.filter((col) => col !== "path").map((col) => `${col} = excluded.${col}`)}
+  `;
+
+  const bind = paramsToBindings(sql, arrayToParams(files));
+
+  return db.exec(sql, { bind });
+}
 
 export interface FileChange {
   path: string;
@@ -17,53 +35,27 @@ export function setRemoteFile(db: Sqlite3.DB, file: FileChange) {
 }
 
 export function setLocalFiles(db: Sqlite3.DB, files: FileChange[]) {
-  if (!files.length) return;
-
-  const sql = `
-INSERT INTO File (path, localContent, meta, localUpdatedAt) VALUES
-${files.map((_, i) => /*reduce query size with shortname*/ `(:p${i}, :c${i}, json(:m${i}), :t${i})`).join(",")}
-ON CONFLICT(path) DO UPDATE SET localContent = excluded.localContent, meta = json(excluded.meta), localUpdatedAt = excluded.localUpdatedAt
-  `;
-
-  const timedFiles = files.map((file) => {
-    const timestamp = file.updatedAt ?? Date.now();
-
-    return {
-      p: file.path,
-      c: file.content,
-      m: JSON.stringify(file.meta),
-      t: timestamp,
-    };
-  });
-
-  const bind = paramsToBindings(sql, arrayToParams(timedFiles));
-
-  return db.exec(sql, { bind });
+  write(
+    db,
+    files.map((file) => ({
+      path: file.path,
+      localContent: file.content,
+      localUpdatedAt: file.updatedAt ?? Date.now(),
+      meta: JSON.stringify(file.meta),
+    }))
+  );
 }
 
 export function setRemoteFiles(db: Sqlite3.DB, files: FileChange[]) {
-  if (!files.length) return;
-
-  const sql = `
-INSERT INTO File (path, remoteContent, meta, remoteUpdatedAt) VALUES
-${files.map((_, i) => /*reduce query size with shortname*/ `(:p${i}, :c${i}, json(:m${i}), :t${i})`).join(",")}
-ON CONFLICT(path) DO UPDATE SET remoteContent = excluded.remoteContent, meta = json(excluded.meta), remoteUpdatedAt = excluded.remoteUpdatedAt
-  `;
-
-  const timedFiles = files.map((file) => {
-    const timestamp = file.updatedAt ?? Date.now();
-
-    return {
-      p: file.path,
-      c: file.content,
-      m: JSON.stringify(file.meta),
-      t: timestamp,
-    };
-  });
-
-  const bind = paramsToBindings(sql, arrayToParams(timedFiles));
-
-  return db.exec(sql, { bind });
+  write(
+    db,
+    files.map((file) => ({
+      path: file.path,
+      remoteContent: file.content,
+      remoteUpdatedAt: file.updatedAt ?? Date.now(),
+      meta: JSON.stringify(file.meta),
+    }))
+  );
 }
 
 export function deleteAllFiles(db: Sqlite3.DB) {
