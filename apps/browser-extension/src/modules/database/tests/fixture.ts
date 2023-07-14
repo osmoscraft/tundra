ce55d50c1dc2bce54fa365283f4558975996bc85
chuanqisun <chuanqi.sun.dev@gmail.com>
2023-07-14 06:22:58 +0000

    refactor sql utils

--- apps/browser-extension/src/modules/database/tests/fixture.ts
+++ apps/browser-extension/src/modules/database/tests/fixture.ts
@@ -1,4 +1,4 @@
-import { arrayToParams, paramsToBindings, sqlite3Mem } from "@tinykb/sqlite-utils";
+import { selectMany, sqlite3Mem, upsertMany } from "@tinykb/sqlite-utils";
 import { assertEqual } from "../../live-test";
 import type { DbFileStatus, DbFileV2Internal, DbFileV2Snapshot } from "../schema";
 
@@ -284,84 +284,4 @@ export function parseState(state: string): ParsedState | null {
     remote,
     synced,
   };
-}
-
-export interface SelectManyInput<T extends {}> {
-  table: string;
-  key: string & keyof T;
-  value: any[];
-}
-export function selectMany<T extends {}>(db: Sqlite3.DB, input: SelectManyInput<T>): (T | undefined)[] {
-  return input.value.map((value) => {
-    return selectOne(db, { table: input.table, key: input.key, value });
-  });
-}
-
-export interface SelectOneInput<T extends {}> {
-  table: string;
-  key: string & keyof T;
-  value: any;
-}
-
-export function selectOne<T extends {}>(db: Sqlite3.DB, input: SelectOneInput<T>): T | undefined {
-  const sql = `SELECT * FROM ${input.table} WHERE ${input.key} = :${input.key}`;
-  const bind = paramsToBindings(sql, { [input.key]: input.value });
-  const file = db.selectObject<T>(sql, bind);
-  return file;
-}
-
-export interface UpsertManyInput<T extends {}> {
-  table: string;
-  key: string & keyof T;
-  rows: T[];
-}
-
-/**
- * Upsert multiple rows of different kind
- */
-export function upsertMany<T extends {}>(db: Sqlite3.DB, input: UpsertManyInput<T>) {
-  return input.rows.map((row) => {
-    return upsertOne(db, { table: input.table, key: input.key, row });
-  });
-}
-
-/**
- * Upsert multiple rows of the same kind
- */
-export function usertBulk<T extends {}>(db: Sqlite3.DB, input: UpsertManyInput<T>) {
-  return upsertInternal(db, input);
-}
-
-export interface UpsertOneInput<T extends {}> {
-  table: string;
-  key: string & keyof T;
-  row: T;
-}
-
-export function upsertOne<T extends {}>(db: Sqlite3.DB, input: UpsertOneInput<T>) {
-  return upsertInternal(db, { table: input.table, key: input.key, rows: [input.row] });
-}
-
-function upsertInternal<T extends {}>(db: Sqlite3.DB, input: UpsertManyInput<T>) {
-  if (!input.rows.length) return;
-
-  const refRow = input.rows[0];
-  const cols = Object.keys(refRow);
-  const nonKeyCols = cols.filter((col) => col !== input.key);
-
-  const sql = [
-    `INSERT INTO ${input.table} (${cols.join(",")}) VALUES`,
-    input.rows.map((_, i) => `(${cols.map((col) => `:${col}${i}`).join(",")})`).join(","),
-    nonKeyCols.length
-      ? `ON CONFLICT(${input.key}) DO UPDATE SET ${cols
-          .filter((col) => col !== input.key)
-          .map((col) => `${col} = excluded.${col}`)}`
-      : "",
-  ]
-    .filter(Boolean)
-    .join("\n");
-
-  // TODO minimize col names to index
-  const bind = paramsToBindings(sql, arrayToParams(input.rows));
-  return db.exec(sql, { bind });
 }
---
+++ packages/sqlite-utils/src/params.ts
@@ -0,0 +1,23 @@
+export function paramsToBindings(sql: string, params: any) {
+  const bindKeys = sql.matchAll(/:([a-zA-Z0-9]+)/g);
+  const bindObject = Object.fromEntries(
+    [...bindKeys].map(([key, variableName]) => [key, getOrThrow(params, variableName)])
+  );
+
+  return bindObject as Sqlite3.Bind;
+}
+
+function getOrThrow(object: any, key: string) {
+  if (object.hasOwnProperty(key)) {
+    return object[key];
+  } else {
+    console.error(`Variable ":${key}" has no matching key "${key}" in object`, object);
+    throw new Error(`Binding key not found`);
+  }
+}
+
+export function arrayToParams(objects: any[]) {
+  return Object.fromEntries(
+    objects.flatMap((object, i) => Object.entries(object).map(([key, value]) => [`${key}${i}`, value]))
+  );
+}
