ad1562bb72a08d7d5fddd7c7a79c2c26e7c0f0bf
chuanqisun <chuanqi.sun.dev@gmail.com>
2023-07-14 07:40:31 +0000

    remove graph layer methods

--- apps/browser-extension/src/modules/database/file-v2.ts
+++ apps/browser-extension/src/modules/database/file-v2.ts
@@ -1,27 +1,5 @@
 import { selectMany, upsertMany } from "@tinykb/sqlite-utils";
-import type { DbFileReadable, DbFileV2ParsedSource, DbWritableFileV2 } from "./schema";
-
-export interface DbParsedWritableFile {
-  path: string;
-  local?: DbFileV2ParsedSource | null;
-  remote?: DbFileV2ParsedSource | null;
-  synced?: DbFileV2ParsedSource | null;
-}
-
-export function upsertParsedFiles(db: Sqlite3.DB, files: DbParsedWritableFile[]) {
-  const rawFiles = files.map((file) => ({
-    path: file.path,
-    ...(file.local && { local: JSON.stringify(file.local) }),
-    ...(file.remote && { remote: JSON.stringify(file.remote) }),
-    ...(file.synced && { synced: JSON.stringify(file.synced) }),
-  }));
-
-  return upsertRawFiles(db, rawFiles);
-}
-
-export function upsertParsedFile(db: Sqlite3.DB, file: DbParsedWritableFile) {
-  return upsertParsedFiles(db, [file]);
-}
+import type { DbReadableFileV2, DbWritableFileV2 } from "./schema";
 
 export function upsertRawFiles(db: Sqlite3.DB, files: DbWritableFileV2[]) {
   return upsertMany<DbWritableFileV2>(db, { table: "FileV2", key: "path", rows: files });
@@ -32,7 +10,7 @@ export function upsertRawFile(db: Sqlite3.DB, file: DbWritableFileV2) {
 }
 
 export function selectFiles(db: Sqlite3.DB, paths: string[]) {
-  return selectMany<DbFileReadable>(db, { table: "FileV2", key: "path", value: paths });
+  return selectMany<DbReadableFileV2>(db, { table: "FileV2", key: "path", value: paths });
 }
 
 export function selectFile(db: Sqlite3.DB, path: string) {
