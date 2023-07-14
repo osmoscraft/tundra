2eb2f1fa1b0f4f3484fc37303a170bfa4dd17139
chuanqisun <chuanqi.sun.dev@gmail.com>
2023-07-14 18:08:41 +0000

    add delete api

--- apps/browser-extension/src/modules/database/file-v2.ts
+++ apps/browser-extension/src/modules/database/file-v2.ts
@@ -1,4 +1,4 @@
-import { selectMany, upsertMany } from "@tinykb/sqlite-utils";
+import { deleteMany, selectMany, upsertMany } from "@tinykb/sqlite-utils";
 import type { DbReadableFileV2, DbWritableFileV2 } from "./schema";
 
 export function upsertRawFiles(db: Sqlite3.DB, files: DbWritableFileV2[]) {
@@ -15,4 +15,12 @@ export function selectFiles(db: Sqlite3.DB, paths: string[]) {
 
 export function selectFile(db: Sqlite3.DB, path: string) {
   return selectFiles(db, [path])[0];
+}
+
+export function deleteFiles(db: Sqlite3.DB, paths: string[]) {
+  return deleteMany(db, { table: "FileV2", key: "path", value: paths, comparator: "GLOB" });
+}
+
+export function deleteFile(db: Sqlite3.DB, path: string) {
+  return deleteFiles(db, [path]);
 }
