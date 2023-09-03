50ae591d9c703e8c372135bcec71ece6fe37358e
Chuanqi Sun <chuanqi.sun.dev@gmail.com>
2023-09-03 05:39:15 +0000

    allow github connection reset

--- apps/browser-extension/src/modules/devtool/devtool-element.ts
+++ apps/browser-extension/src/modules/devtool/devtool-element.ts
@@ -1,6 +1,7 @@
 import type { AsyncProxy } from "@tundra/rpc-utils";
 import type { DataWorkerRoutes } from "../../workers/data-worker";
 import { clearKeyBindingsCache } from "../settings/key-bindings";
+import { clearGithubConnection } from "../sync/github/github-config";
 import template from "./devtool-element.html";
 import { downloadFile } from "./download-file";
 
@@ -26,6 +27,11 @@ export class DevtoolElement extends HTMLElement {
     this.menu.addEventListener("click", async (e) => {
       const action = (e.target as HTMLElement).closest("[data-action]")?.getAttribute("data-action");
       switch (action) {
+        case "clear-github-connection": {
+          clearGithubConnection();
+          location.reload();
+          break;
+        }
         case "check-health": {
           // set checkhealth=true in the url to check health
           location.search = new URLSearchParams({ checkhealth: "true" }).toString();
